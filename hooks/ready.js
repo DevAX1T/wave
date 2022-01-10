29eb1d68b290cd47f6603d96ddf91de6444d5abd
DevAX1T <darkweaponwaffles@gmail.com>
2022-01-10 14:04:50 -0800

    v1.0.1

--- hooks/ready.js
+++ hooks/ready.js
@@ -8,7 +8,7 @@ module.exports = {
         // set global.botUptime to the current unix time in seconds
         global.botUptime = Math.floor(Date.now() / 1000);
         global.clientDomain = isWindows ? 'localhost' : 'https://devax1t.xyz';
-        client.version = '1.0.0' + (isWindows ? '-dev' : ''); // BOTVERSION
+        client.version = '1.0.1' + (isWindows ? '-dev' : ''); //Bot Version
         client.cache = {};
         // Connect to noblox.js
         noblox.setCookie(settings.cookie).then((user) => {
